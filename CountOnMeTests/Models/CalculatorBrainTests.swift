2774a2931162e09876e9c99421f06116134b30d9
orangebird-boop <70488211+orangebird-boop@users.noreply.github.com>
2021-10-20 16:45:06 +0200

    last commit

--- CountOnMeTests/Models/CalculatorBrainTests.swift
+++ CountOnMeTests/Models/CalculatorBrainTests.swift
@@ -13,15 +13,7 @@ class SimpleCalcTests: XCTestCase {
     override func setUpWithError() throws {
         try super.setUpWithError()
     }
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
+    
     let calculatorBrain = CalculatorBrain()
 
     func testSimpleAddition() {
